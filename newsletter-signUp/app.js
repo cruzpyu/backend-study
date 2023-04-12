4e947f9f626cf62f260f586f60878c9f90200a25
Yullie Cruz <yullie_cruz@ecglobal.com>
2023-04-12 18:02:49 -0300

    add initial files

--- newsletter-signUp/app.js
+++ newsletter-signUp/app.js
@@ -4,4 +4,19 @@ const request = require("request");
 
 const app = express();
 
-app.listen(3000, () => "server is running on port 3000");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + "/"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const email = req.body.email;
+  res.send(console.log(firstName, lastName, email));
+});
+
+app.listen(3000, () => console.log("server is running on port 3000"));
